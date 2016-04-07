bcf6f923c0876093e7ae905b21c43822b781b906
sellylu <s83721456@gmail.com>
2016-04-08 00:51:17 +0800

    new select parser

--- src/Select.java
+++ src/Select.java
@@ -13,36 +13,29 @@ public class Select extends SQLRequest{
 		input = input.replaceAll(", ", ",").toLowerCase();
 										
 		this.colName = input.split(" ",2)[1].split("from")[0].trim().split(",");
-		String from = input.split("from")[1];
-		if(from.contains("inner join")) {
-			this.tableName = from.split("inner join")[0].trim().split(",");
-		} else if(from.contains("where")) {
+		String from = input.split("from")[1].trim();
+		if(from.contains("where")) {
 			this.tableName = from.split("where")[0].trim().split(",");
+		} else {
+			this.tableName = from.split(",");
 		}
-		if(input.contains("inner join")) {
-			this.join = input.split("join")[1].trim();
-			if(this.join.contains("where"))
-				this.join = this.join.split("where")[0].trim();
-		}
-		
+	
 		if(input.contains("where"))
 			this.condition = input.split("where")[1].trim();
 		
-
-		for(String s:this.colName)
+		for(String s: colName) 
 			System.out.println(s);
-		for(String s:this.tableName)
+		for(String s: tableName)
 			System.out.println(s);
-		System.out.println(this.condition);
-		System.out.println(this.join);
-		
-		
+		System.out.println(condition);
 		
 	}
+	
+	
 	
 	public String[] colName;
 	public String[] tableName;
 	public String condition;
-	public String join;
+	
 
 }

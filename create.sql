ad2c3d335165ab3d26385d24042645d404b663fe
sellylu <s83721456@gmail.com>
2016-04-20 23:43:57 +0800

    debug: null cs.datatype error

--- create.sql
+++ create.sql
@@ -1,13 +1,17 @@
 create table tweets(
-twid int primary key,
-tweet varchar(30),
-utcDate varchar(30),
-city varchar(30),
-userId int);
+	twid int primary key,
+	tweet varchar(30),
+	utcDate varchar(30),
+	city varchar(30),
+	userId int
+);
+
 import tweets.sql;
 
 create table user1 (
-userId int primary key,
-name varchar(30),
-userLocation varchar(30));
+	userId int primary key,
+	name varchar(30),
+	userLocation varchar(30)
+);
+
 import user1.sql;

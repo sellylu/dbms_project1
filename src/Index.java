27d691546aafc35a24bbfa0611f58d709bea43e9
liutzuyuan <yad50968@gmail.com>
2016-05-06 20:09:04 +0800

    add Index.java

---
+++ src/Index.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class IndexList{
+	
+	List<Index> indexList;
+	public IndexList(){
+		indexList = new ArrayList<Index>();
+	}
+}
+
+
+public class Index {
+	private String tablename;
+	private String colname;
+	private BTree btree;
+	public Index(String tablename , String colname){
+		this.tablename = tablename;
+		this.colname = colname;
+		btree = new BTree();
+	}
+	
+}

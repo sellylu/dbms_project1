8d0140ff9255bd2fa07f74abb63138437ed4afb4
liutzuyuan <yad50968@gmail.com>
2016-05-08 10:33:40 +0800

    add IndexList.java

--- src/Index.java
+++ src/Index.java
@@ -3,39 +3,10 @@ import java.util.List;
 
 
 
-class IndexList {
-	List<Index> list;
-	public IndexList(){
-		list = new ArrayList<Index>();
-		
-	}
-	
-	public Boolean checkIndex(String tablename,String colname){
-		for(Index i : list){
-			if(i.tablename.equals(tablename) && i.colname.equals(colname)){
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public Index getIndex(String tablename,String colname){
-		for(Index i : list){
-			if(i.tablename.equals(tablename) && i.colname.equals(colname)){
-				return i;
-			}
-		}
-		return null;
-	}
-	
-	public void addIndex(Index index){
-		list.add(index);
-	}
-}
 public class Index {
-	protected String tablename;
-	protected String colname;
-	protected String indexname;
+	public String tablename;
+	public  String colname;
+	public String indexname;
 	
 	public BTree btree;
 	public Index(String indexname,String tablename , String colname){
@@ -45,8 +16,6 @@ public class Index {
 		this.indexname = indexname;
 		btree = new BTree();
 	}
-	public void addPointer(){
-		
-	}
+	
 	
 }
---
+++ src/IndexList.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class IndexList {
+	List<Index> list;
+	public IndexList(){
+		list = new ArrayList<Index>();
+		
+	}
+	
+	public Boolean checkIndex(String tablename,String colname){
+		for(Index i : list){
+			if(i.tablename.equals(tablename) && i.colname.equals(colname)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Index getIndex(String tablename,String colname){
+		for(Index i : list){
+			if(i.tablename.equals(tablename) && i.colname.equals(colname)){
+				return i;
+			}
+		}
+		return null;
+	}
+	
+	public void addIndex(Index index){
+		list.add(index);
+	}
+}
